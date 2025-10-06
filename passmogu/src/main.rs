4871efef2dfebf29bf924b25d70541cb537dd216
josh <joshzhaa@gmail.com>
2025-10-06 04:19:41 +0000

    example cli scaffolding

--- passmogu/src/main.rs
+++ passmogu/src/main.rs
@@ -1,7 +1,17 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    name: String,
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
-    let args: Vec<String> = std::env::args().collect();
-    for arg in args {
-        println!("{arg}");
+    let args = Args::parse();
+    for _ in 0..args.count {
+        println!("yo {}!", args.name)
     }
 }
