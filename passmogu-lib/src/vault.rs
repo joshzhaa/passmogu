a347cff55f8ef20058e9af30e24a539237675e5e
josh <joshzhaa@gmail.com>
2025-10-13 19:37:16 +0000

    use Box slices to disallow in place modification of form data

--- passmogu-lib/src/vault.rs
+++ passmogu-lib/src/vault.rs
@@ -4,24 +4,24 @@ use std::ops::Index;
 #[derive(Debug, PartialEq, Eq)]
 pub struct Field {
     /// e.g. "username", "password", "What's your mother's maiden name?"
-    pub prompt: String,
+    pub prompt: Box<str>,
     /// value to populate into field
-    pub answer: String,
+    pub answer: Box<str>,
 }
 
 /// A form is simply a collection of fields to populate.
-/// All vectors of fields consitute a form, so we simply define a type alias here.
-type Form = Vec<Field>;
+/// Any list of fields consitutes a valid form, so we simply define a type alias here.
+type Form = Box<[Field]>;
 
 /// HashMap mapping form_name -> form, serializable to and from plaintext strings in a tsv format
 /// (which obviously disallows including \t in any fields).
 /// The format is "form_name\tprompt1\tanswer1\tprompt2\tanswer2\n". The empty Vault is "" (not "\n").
 #[derive(Debug, PartialEq, Eq)]
-pub struct Vault(pub HashMap<String, Form>);
+pub struct Vault(pub HashMap<Box<str>, Form>);
 
 impl Vault {
-    /// serialize to String
-    pub fn dump(&self) -> String {
+    pub fn dump(&self) -> Box<str> {
+        // Could also parse twice to allocate the right size, then to populate, but it's easier this way.
         let mut table = String::new();
         for (name, form) in &self.0 {
             table.push_str(name);
@@ -33,10 +33,9 @@ impl Vault {
             }
             table.push('\n');
         }
-        table
+        table.into_boxed_str()
     }
 
-    /// deserialize from &str
     pub fn load(data: &str) -> Option<Vault> {
         let mut vault = Vault(HashMap::new());
         for row in data.split('\n') {
@@ -45,16 +44,16 @@ impl Vault {
             }
             let mut i = row.split('\t');
             // expects name\tprompt\tanswer\tprompt\tanswer...
-            let name = i.next()?.to_string(); // each row must have a form name as the first token
-            let mut form = Form::new();
+            let name = i.next()?; // each row must have a form name as the first token
+            let mut form = Vec::new();
             while let Some(prompt) = i.next() {
                 let answer = i.next()?; // each prompt must be paired with an answer
                 form.push(Field {
-                    prompt: prompt.to_string(),
-                    answer: answer.to_string(),
+                    prompt: prompt.into(),
+                    answer: answer.into(),
                 });
             }
-            vault.0.insert(name, form);
+            vault.0.insert(name.into(), form.into());
         }
         Some(vault)
     }
@@ -76,36 +75,33 @@ mod tests {
     fn empty_vault() {
         let serialized = "";
         let vault = Vault::load(serialized).unwrap();
-        println!("{vault:?}");
         assert_eq!(vault.0.len(), 0);
-        assert_eq!(vault.dump(), serialized);
+        assert_eq!(*vault.dump(), *serialized);
     }
 
     #[test]
     fn basic_vault() {
-        let serialized = "irc\tusername\tAzureDiamond\tpassword\thunter2\tWhat was the name of your best friend in elementary school?\tCthon98\nother website dot com\tusername\tCthon98\tpassword\t*********\tWhat was the name of your best friend in elementary school?\tAzureDiamond\n";
+        let serialized = "irc\tusername\tAzureDiamond\tpassword\thunter2\tWho's your best friend?\tCthon98\nother website dot com\tusername\tCthon98\tpassword\t*********\tWho's your best friend?\tAzureDiamond\n";
         let vault = Vault::load(serialized).unwrap();
         let first_form = &vault["irc"];
-        assert_eq!(first_form[0].prompt, "username");
-        assert_eq!(first_form[0].answer, "AzureDiamond");
-        assert_eq!(first_form[1].prompt, "password");
-        assert_eq!(first_form[1].answer, "hunter2");
-        assert_eq!(
-            first_form[2].prompt,
-            "What was the name of your best friend in elementary school?"
-        );
-        assert_eq!(first_form[2].answer, "Cthon98");
+        assert_eq!(*first_form[0].prompt, *"username");
+        assert_eq!(*first_form[0].answer, *"AzureDiamond");
+        assert_eq!(*first_form[1].prompt, *"password");
+        assert_eq!(*first_form[1].answer, *"hunter2");
+        assert_eq!(*first_form[2].prompt, *"Who's your best friend?");
+        assert_eq!(*first_form[2].answer, *"Cthon98");
 
         let second_form = &vault["other website dot com"];
-        assert_eq!(second_form[0].prompt, "username");
-        assert_eq!(second_form[0].answer, "Cthon98");
-        assert_eq!(second_form[1].prompt, "password");
-        assert_eq!(second_form[1].answer, "*********");
-        assert_eq!(
-            second_form[2].prompt,
-            "What was the name of your best friend in elementary school?"
-        );
-        assert_eq!(second_form[2].answer, "AzureDiamond");
+        assert_eq!(*second_form[0].prompt, *"username");
+        assert_eq!(*second_form[0].answer, *"Cthon98");
+        assert_eq!(*second_form[1].prompt, *"password");
+        assert_eq!(*second_form[1].answer, *"*********");
+        assert_eq!(*second_form[2].prompt, *"Who's your best friend?");
+        assert_eq!(*second_form[2].answer, *"AzureDiamond");
+
+        // best friends
+        assert_eq!(first_form[0].answer, second_form[2].answer);
+        assert_eq!(first_form[2].answer, second_form[0].answer);
 
         assert_eq!(Vault::load(&vault.dump()), Vault::load(serialized));
     }
