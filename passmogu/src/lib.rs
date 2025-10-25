e512ac763efc73b3394e0ac04fc35240a538e311
josh <joshzhaa@gmail.com>
2025-10-25 20:25:05 +0000

    reorder some modules

--- passmogu/src/lib.rs
+++ passmogu/src/lib.rs
@@ -1,10 +1,10 @@
-// interface for storing login form data in plaintext
-pub mod vault;
 // password based encryption
 pub mod encrypt;
 // password generation
 pub mod generate;
-// hex enc/dec
-pub mod hex;
 // zeroizing string type
 pub mod secret;
+// interface for serializing login form data
+pub mod vault;
+// hex encoding / decoding
+mod hex;
